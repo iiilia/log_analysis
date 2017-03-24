fee3b645f01050e55e72632057753b7d41190e83
Ilya Ivanitskiy <iiivanitskiy@avito.ru>
2017-03-24 16:50:16 +0300

    add description for agg logic

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 import re
+from datetime import time
+
 stop_regex = re.compile(u"(SQLProxy)|(P2_COD)", flags=re.UNICODE)
 
 def line_processing(line):
@@ -13,13 +15,18 @@ def line_processing(line):
     if len(stop_regex.findall(line)) > 0:
         result = dict()
         line_temp = line.split(u'|')
-        time = line_temp[0]
+        log_time = line_temp[0]
         line = u'|'.join(line_temp[1:]).split(u';')
 
-    return result
+        time_to_seconds = log_time.split(u'.')[0]
+        id = line[0]
+        result['time'] = time.strftime(log_time, '%h:%m:%s.%f')
+
+    return (time_to_seconds, id, result)
 
 
 def __main__(*argv):
+
     if argv and argv[0] > 2:
         path_to_data = argv[1]
 
@@ -32,6 +39,16 @@ def __main__(*argv):
         f_out = open(path_to_result, 'w', encoding='utf8')
 
         for line in f:
+            # для каждого момента времени час-минута-секунда имеем структуру со следующими атрибутами
+            #   1 количество запросов
+            #   2 количество ответов
+            #   3 из этого ли момента времени приходят сейчас запросы? или мы можем получить еще новых
+
+            # Если количество запросов == количество ответов и 3 == False -> считаем статистику
+            # по этому моменту и пишем ее в файл
+
+            # нужно проверить, что секунда пишутся последовательно
+            
             line_processed = line_processing(line)
 
         # тут что то происходит
