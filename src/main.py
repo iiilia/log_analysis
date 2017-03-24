28315e8954151163f55fee8e167a5cad5b2af4ad
Ilya Ivanitskiy <iiivanitskiy@avito.ru>
2017-03-24 18:45:56 +0300

    almost ready (TODO fixes)

--- src/main.py
+++ src/main.py
@@ -14,14 +14,27 @@ def process_result(time_type_structure, f_out, last_time_write):
                 and not in_second_structure.get('now_or_past', True) \
                 and time.strftime(key[0], '%h:%m:%s') == (time.strftime(last_time_write, '%h:%m:%s') + \
                                                                   timedelta(seconds=1)):
-            # это условие нужно переписать нормально (ПРОВЕРИТЬ)
-            result_line = count_stat_by_second(in_second_structure)
+            # TODO это условие нужно переписать нормально (ПРОВЕРИТЬ)
+            result_line = count_stat_by_second(in_second_structure, key)
             f_out.write(result_line)
+            # TODO удалить по ключу (время, тип) из словаря
 
-
-def count_stat_by_second(in_second_structure):
+def count_stat_by_second(in_second_structure, info):
     res = list()
     # считаем статистику
+    res.append(info[0])
+    res.append(info[1])
+    res.append(in_second_structure['count_in'])
+    latency = []
+    for key_ in list(in_second_structure.keys()):
+        if key_ not in [u'count_in', u'count_out', u'now_or_past']:
+            # TODO  проверить что говно снизу возвращает миллисекунды
+            latency.append(in_second_structure[key_]['a'] - in_second_structure[key_]['q'])
+    res.append(np.mean(latency))
+    res.append(np.median(latency))
+    res.append(np.percentile(latency, 90))
+    res.append(np.percentile(latency, 99))
+    res.append(max(latency))
     return u';'.join([str(x) for x in res])
 
 
@@ -32,8 +45,6 @@ def line_processing(line):
 
     global stop_regex
 
-    result = None
-
     if len(stop_regex.findall(line)) > 0:
         line_temp = line.split(u'|')
         log_time = line_temp[0]
@@ -41,19 +52,23 @@ def line_processing(line):
 
         time_to_seconds = log_time.split(u'.')[0]
         id = line[0]
-        # получение типа сообщения
+        # TODO получение типа сообщения
         type_of_line = u''
+        # TODO запрос или ответ???
         q_or_a = u''
         time_full = time.strftime(log_time, '%h:%m:%s.%f')
 
-    return (time_to_seconds, id, type_of_line, q_or_a, time_full)
+        return (time_to_seconds, id, type_of_line, q_or_a, time_full)
+
+    else:
+        return None
 
 
 def __main__(*argv):
     if argv and argv[0] > 2:
         path_to_data = argv[1]
 
-        # путь к файлу с результатами
+        # TODO путь к файлу с результатами
         path_to_result = u''
 
         # нам не указана кодировка файла
@@ -104,7 +119,6 @@ def __main__(*argv):
 
                 if q_or_a == u'q':
                     count_in += 1
-                    # проверяем что запрос пришел из новой секунды
                 elif q_or_a == 'a':
                     count_out += 1
 
